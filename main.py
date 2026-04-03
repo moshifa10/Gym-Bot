05b91c58ddd93c154540950cb12b0ad3eaf1affc
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-04-03 16:40:56 +0200

    added keys I will use

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import os
 web = webdriver.ChromeOptions()
 web.add_experimental_option("detach", True)
 
+user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
+web.add_argument(f"--user-data-dir={user_data_dir}")
 
 driver = webdriver.Chrome(options=web)
-driver.get("https://ozh.github.io/cookieclicker/")
-
-user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
+driver.get(os.getenv(key="GYM_URL"))
