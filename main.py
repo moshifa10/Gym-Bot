25f2dd7239d7a47d9bd3711b5736e05e61d6732b
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-04-03 20:15:44 +0200

    made it to choose a day that is available and click it if it finds it

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 import os
 import dotenv
+import datetime
 
 dotenv.load_dotenv()
 
@@ -36,3 +37,26 @@ email.send_keys(ACCOUNT_EMAIL)
 password.send_keys(ACCOUNT_PASSWORD)
 login = driver.find_element(By.ID, "submit-button")
 login.click()
+
+
+today = datetime.date.today()
+day = today.strftime("%A").lower()
+
+tuesday = None
+if day != "tuesday":
+    for i in range(1, 8):
+        next_day = today + datetime.timedelta(days=8)
+        if next_day.strftime("%A").lower() == "tuesday":
+            tuesday = next_day
+else: tuesday = today # 2026-04-04
+
+# Find the class 
+driver.implicitly_wait(2)
+# class_find = driver.find_element(By.ID, f"class-time-spin-{tuesday}-1800")
+book_button =driver.find_element(By.ID, f"book-button-spin-{tuesday}-1800")
+book_button.click()
+print(f"✓ Booked: Spin Class on {tuesday.strftime("%a")}, {tuesday.strftime("%B")} {tuesday.strftime("%m")}")
+
+
+
+
